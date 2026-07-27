c64c70cb12ac3374f33b17e58a68f34ecc83b49e
Moaaz Osman <140607735+moaazosman@users.noreply.github.com>
2026-07-27 13:34:04 +0200

    Add data analysis libraries for churn analysis

--- scripts/demographic_churn_analysis.py
+++ scripts/demographic_churn_analysis.py
@@ -1,4 +1,8 @@
 # 1. Demographic vs Churn
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 total_customers = len(df)
 total_churners  = df[df["Churn Label"] == "Yes"].shape[0]
